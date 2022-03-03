6a2e7ea6b4d1a14a9bf5ca55950763035033292d
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-03 14:58:00 +0800

    changed position

--- pages/SSRpokemon/index.js
+++ pages/SSRpokemon/index.js
@@ -3,13 +3,33 @@ import { ListPokemons } from "../../Components/dataFetching";
 import SSRPokemonList from "../../Components/SSRPokemonList";
 import Layout from "../../Components/Layouts/layout";
 
+export async function getServerSideProps() {
+  const pokemonData = [];
+
+  ///Fetch the result from the endpoint
+  const data = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?offset=300&limit=100"
+  );
+  // const jsonDataResult = await data.json();
+  const { results } = await data.json();
+
+  ///Check if the jsonDataResult has data and loop through all the result to
+  ///get the url's and then fetch every single data from the url
+  // if (results) {
+  for (const element of results) {
+    const data = await fetch(`${element.url}`);
+    const jsonData = await data.json();
+    pokemonData.push(jsonData);
+  }
+  // }
+  return {
+    props: {
+      data: pokemonData,
+    },
+  };
+}
+
 function SSRPokemon({ data }) {
-  // const router = useRouter();
-  // const {
-  //   query: { loading },
-  // } = router;
-  // // const loading = router.query({ loading });
-  // console.log(loading);
   return (
     <div style={{ height: "100%" }}>
       <Container width="inherit" pt={10}>
@@ -24,29 +44,3 @@ export default SSRPokemon;
 SSRPokemon.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
-
-export async function getServerSideProps() {
-  const pokemonData = [];
-
-  ///Fetch the result from the endpoint
-  const data = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100&offset=300"
-  );
-  // const jsonDataResult = await data.json();
-  const { results } = await data.json();
-
-  ///Check if the jsonDataResult has data and loop through all the result to
-  ///get the url's and then fetch every single data from the url
-  // if (results) {
-    for (const element of results) {
-      const data = await fetch(`${element.url}`);
-      const jsonData = await data.json();
-      pokemonData.push(jsonData);
-    }
-  // }
-  return {
-    props: {
-      data: pokemonData,
-    },
-  };
-}
