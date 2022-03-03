9677fad27e70f5e22f1e381bc9b2a5d11e37b8b8
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-03 15:25:12 +0800

    edited the link

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
                 gap={2}
               >
                 {/*Button Section For Navigating to SSR Pokemon List */}
-                <Link href="/ssrpokemon" passHref>
+                <Link href="/ssr/pokemon" passHref>
                   <Button
                     // onClick={() => navigateToSSR()}
                     borderColor="tomato"
@@ -91,7 +91,7 @@ export default function Home() {
                 </Link>
 
                 {/*Button Section For Navigating to SSG Pokemon List */}
-                <Link passHref href="/ssgpokemon">
+                <Link passHref href="/ssg/pokemon">
                   <Button
                     border="1px"
                     borderColor="tomato"
