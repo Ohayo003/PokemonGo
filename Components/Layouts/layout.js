61ac62f6a26577ac01178e293503e42f4e6d4112
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-03 19:14:28 +0800

    changed the background

--- Components/Layouts/layout.js
+++ Components/Layouts/layout.js
@@ -1,13 +1,18 @@
 import Header from "../header.js";
 import Image from "next/image";
 import { Box } from "@chakra-ui/react";
-import pokeballbg from "../../public/pokeball-bg.jpg";
+import pokeballbg from "../../public/pokemon-bg.jpg";
 
 function Layout({ children }) {
   return (
     <Box width="100%" position="relative" zIndex="0">
       <Box width="100%" height="100%" position="absolute" zIndex="-1">
-        <Image src={pokeballbg} alt="background" layout="fill" objectFit="cover" />
+        <Image
+          src={pokeballbg}
+          alt="background"
+          layout="fill"
+          objectFit="cover"
+        />
       </Box>
       <Header />
       {children}
