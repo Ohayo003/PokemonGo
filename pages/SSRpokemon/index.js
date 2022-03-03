6498b1f46ca9f666accc7cb132ba598e0afbc6dd
John Christopher Go <stephengabrielgo@gmail.com>
2022-03-03 15:03:37 +0800

    edit

--- pages/SSRpokemon/index.js
+++ pages/SSRpokemon/index.js
@@ -7,18 +7,18 @@ export async function getServerSideProps() {
   const pokemonData = [];
 
   ///Fetch the result from the endpoint
-  const data = await fetch(
+  const data1 = await fetch(
     "https://pokeapi.co/api/v2/pokemon?offset=300&limit=100"
   );
   // const jsonDataResult = await data.json();
-  const { results } = await data.json();
+  const { results } = await data1.json();
 
   ///Check if the jsonDataResult has data and loop through all the result to
   ///get the url's and then fetch every single data from the url
   if (results) {
     for (const element of results) {
-      const data = await fetch(`${element.url}`);
-      const jsonData = await data.json();
+      const data2 = await fetch(`${element.url}`);
+      const jsonData = await data2.json();
       pokemonData.push(jsonData);
     }
   }
